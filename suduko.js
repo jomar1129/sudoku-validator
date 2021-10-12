f0f7a171e166ea89c47510d5cac5f6b5c43342e1
jomar1129 <jomarcotejo@gmail.com>
2021-10-12 21:52:17 +1100

    added validation will not accept letters

--- suduko.js
+++ suduko.js
@@ -10,57 +10,72 @@ const isValidSudoku = (board) => {
   const validated = [];
   const grids = [];
 
-  let isValidChar = true;
+  let validCounter = 0;
 
-  board.forEach((row, rowIndex) => {
-    /* :: push if row is validated ::*/
-
-    validated.push(isEmpty(row));
-
-    /* check validation for column then push to its own array */
-    const column = [];
-    for (let columnIndex = 0; columnIndex < board.length; columnIndex++) {
-      column.push(board[columnIndex][rowIndex]);
-    }
-    validated.push(isEmpty(column));
-
-    //grids
-    grids.push([]);
-  });
-
-  //grids
-  board.forEach((row, rowIndex) => {
-    row.forEach((character, charIndex) => {
-      let gridRow = 0;
-      /*:: increase grid row until 2 ::*/
-
-      if (rowIndex >= 3 && rowIndex <= 5) {
-        gridRow = 1;
-      } else if (rowIndex >= 6 && rowIndex <= 8) {
-        gridRow = 2;
+  board.forEach((row, index) => {
+    row.map((d) => {
+      /* :: check if value is a letter ::*/
+      var reg = /^-?\d*\.?\d*$/;
+      if (!d.match(reg)) {
+        console.log(d);
+        validCounter += 1;
       }
-
-      /* :: between 3 and 5 and gridrow must increment to 3 :: */
-      if (charIndex >= 3 && charIndex <= 5) {
-        gridRow += 3;
-      } else if (charIndex >= 6 && charIndex <= 8) {
-        gridRow += 6;
-      }
-
-      grids[gridRow].push(character);
     });
   });
 
-  /* :: to check if the grid has the correct inputs ::*/
-  // test purposes
+  if (validCounter === 0) {
+    board.forEach((row, rowIndex) => {
+      /* :: push if row is validated ::*/
 
-  // console.table(grids);
+      validated.push(isEmpty(row));
 
-  grids.forEach((grid) => {
-    validated.push(isEmpty(grid));
-  });
+      /* check validation for column then push to its own array */
+      const column = [];
+      for (let columnIndex = 0; columnIndex < board.length; columnIndex++) {
+        column.push(board[columnIndex][rowIndex]);
+      }
+      validated.push(isEmpty(column));
 
-  return validated.every((value) => value === true);
+      //grids
+      grids.push([]);
+    });
+
+    //grids
+    board.forEach((row, rowIndex) => {
+      row.forEach((character, charIndex) => {
+        let gridRow = 0;
+        /*:: increase grid row until 2 ::*/
+
+        if (rowIndex >= 3 && rowIndex <= 5) {
+          gridRow = 1;
+        } else if (rowIndex >= 6 && rowIndex <= 8) {
+          gridRow = 2;
+        }
+
+        /* :: between 3 and 5 and gridrow must increment to 3 :: */
+        if (charIndex >= 3 && charIndex <= 5) {
+          gridRow += 3;
+        } else if (charIndex >= 6 && charIndex <= 8) {
+          gridRow += 6;
+        }
+
+        grids[gridRow].push(character);
+      });
+    });
+
+    /* :: to check if the grid has the correct inputs ::*/
+    // test purposes
+
+    //   console.table(grids);
+
+    grids.forEach((grid) => {
+      validated.push(isEmpty(grid));
+    });
+
+    return validated.every((value) => value === true);
+  } else {
+    return "Can't Accept Letters on input";
+  }
 };
 
 module.exports = isValidSudoku;
