d0a6a66dbc16bc623dc5bca2d6bd278076ca7e96
jomar1129 <jomarcotejo@gmail.com>
2021-10-12 21:30:31 +1100

    added suduko js and function for suduko

---
+++ suduko.js
@@ -0,0 +1,66 @@
+/*:: Create a Function where it filters out empyty Spaces ::*/
+
+const isEmpty = (array) => {
+  /* :: Filter out Empty Spaces of the array :: */
+  const digits = array.filter((character) => character !== ".");
+  return digits.length === [...new Set(digits)].length;
+};
+
+const isValidSudoku = (board) => {
+  const validated = [];
+  const grids = [];
+
+  let isValidChar = true;
+
+  board.forEach((row, rowIndex) => {
+    /* :: push if row is validated ::*/
+
+    validated.push(isEmpty(row));
+
+    /* check validation for column then push to its own array */
+    const column = [];
+    for (let columnIndex = 0; columnIndex < board.length; columnIndex++) {
+      column.push(board[columnIndex][rowIndex]);
+    }
+    validated.push(isEmpty(column));
+
+    //grids
+    grids.push([]);
+  });
+
+  //grids
+  board.forEach((row, rowIndex) => {
+    row.forEach((character, charIndex) => {
+      let gridRow = 0;
+      /*:: increase grid row until 2 ::*/
+
+      if (rowIndex >= 3 && rowIndex <= 5) {
+        gridRow = 1;
+      } else if (rowIndex >= 6 && rowIndex <= 8) {
+        gridRow = 2;
+      }
+
+      /* :: between 3 and 5 and gridrow must increment to 3 :: */
+      if (charIndex >= 3 && charIndex <= 5) {
+        gridRow += 3;
+      } else if (charIndex >= 6 && charIndex <= 8) {
+        gridRow += 6;
+      }
+
+      grids[gridRow].push(character);
+    });
+  });
+
+  /* :: to check if the grid has the correct inputs ::*/
+  // test purposes
+
+  // console.table(grids);
+
+  grids.forEach((grid) => {
+    validated.push(isEmpty(grid));
+  });
+
+  return validated.every((value) => value === true);
+};
+
+module.exports = isValidSudoku;
